2c173dc6e74962d3bd75236cbe49388ed90de73b
SK <itzmesk2000@gmail.com>
2024-10-06 23:46:06 -0700

    Update Valid Perfect Square.py

--- Binary-Search-Question/Valid Perfect Square.py
+++ Binary-Search-Question/Valid Perfect Square.py
@@ -1,2 +1,15 @@
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
+        l=0
+        r=num
+        while (l<=r):
+            m=(l+r)//2
+            m_square=m*m
+            if (m_square == num):
+                return True
+            elif (m_square<num):
+                r=m-1
+            else: 
+                l=m+1
+        return False
+                
