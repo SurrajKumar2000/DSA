e8511158ae73c2b668f42fdd18a14942017fff5c
SK <itzmesk2000@gmail.com>
2024-10-09 05:13:02 -0700

    Update Rotate Image.py

--- Rotate Image.py
+++ Rotate Image.py
@@ -1,6 +1,6 @@
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
-            n= len(matrix)
+        n= len(matrix)
         for i in range(n):
             for j in range(i+1,n):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
