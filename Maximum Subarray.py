6e667aa74340b571b9772170b7fc9aa2c89b28bb
SK <itzmesk2000@gmail.com>
2024-10-09 05:14:22 -0700

    Update Maximum Subarray.py

--- Maximum Subarray.py
+++ Maximum Subarray.py
@@ -1,1 +1,9 @@
-
+cur_sum = 0
+        max_sum = float('-inf')
+        for i in range (len(nums)):
+            cur_sum +=nums[i]
+            max_sum=max(cur_sum,max_sum)
+            if (cur_sum<0):
+                cur_sum=0
+        return max_sum
+        
