ff3c286f17b8c30938e95743b61508f0bd7b3001
SK <itzmesk2000@gmail.com>
2024-10-09 05:12:19 -0700

    Update Rotate Image.py

--- Rotate Image.py
+++ Rotate Image.py
@@ -1,1 +1,10 @@
 
+n= len(matrix)
+
+        for i in range(n):
+            for j in range(i+1,n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+                
+        for i in range(n):
+            for j in range(n // 2):
+                matrix[i][j], matrix[i][n-j-1] = matrix[i][n-j-1], matrix[i][j]
