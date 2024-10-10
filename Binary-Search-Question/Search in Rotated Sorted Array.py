dfab69ea884ed31f9d8507a8280a082c090d3816
SK <itzmesk2000@gmail.com>
2024-10-09 17:48:50 -0700

    Update Search in Rotated Sorted Array.py

--- Binary-Search-Question/Search in Rotated Sorted Array.py
+++ Binary-Search-Question/Search in Rotated Sorted Array.py
@@ -1,5 +1,5 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        for i not in target:
+        if i not in target:
             return -1
         return nums.index(target)
