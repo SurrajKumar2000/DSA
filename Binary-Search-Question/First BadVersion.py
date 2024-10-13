aa6ab1409bacf2bc44b27798f097c60552396edd
SK <itzmesk2000@gmail.com>
2024-10-12 23:39:30 -0700

    Update First BadVersion.py

--- Binary-Search-Question/First BadVersion.py
+++ Binary-Search-Question/First BadVersion.py
@@ -2,3 +2,9 @@ class Solution:
     def firstBadVersion(self, n: int) -> int:
         l,r = 0,n
         while(l<r):
+            m = (l+r)//2
+            if isBadVersion(m):
+                m = r
+            else:
+                l = m+1
+                
