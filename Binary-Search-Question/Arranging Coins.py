ffade374e73f094565698c658ecae3d851cc998c
SK <itzmesk2000@gmail.com>
2024-10-16 05:36:28 -0700

    Update Arranging Coins.py

--- Binary-Search-Question/Arranging Coins.py
+++ Binary-Search-Question/Arranging Coins.py
@@ -1,2 +1,13 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
+         l,r=0,n
+        while l<=r:
+            m=l+(r-l)//2
+            coins = ((m)*(m+1))//2
+            if coins==n:
+                return m
+            elif coins<n:
+                l = m+1
+            else:
+                r = m-1
+        return r
