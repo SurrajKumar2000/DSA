93157aad808a19b932812bcefea25336c0701e5a
SK <itzmesk2000@gmail.com>
2024-10-19 23:14:25 -0700

    Update Sort Colors.py

--- Sort Colors.py
+++ Sort Colors.py
@@ -1,10 +1,11 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
 
-        count = [0,0,0]
-        for color in nums:
-            count[color] += 1
-        r,w,b = count
-        nums[:r] = [0]*r
-        nums[r:r+w] = [1]*w
-        nums[:r+w] = [2]*b
+        counts = [0,0,0]
+        for colors in nums:
+            counts[colors]+=1
+
+        r,w,b = counts
+        nums[:r] = [0] * r
+        nums[r:r+w] = [1] * w
+        nums[r+w:] = [2] * b
