838a795f3a8f2265f5271899fa882eaef2c314ca
SK <itzmesk2000@gmail.com>
2024-10-21 23:25:10 -0700

    Update 2 Sum.py

--- 2 Sum.py
+++ 2 Sum.py
@@ -3,6 +3,6 @@ class Solution:
 
         for i in range(n):
             for j in range(i+1,n):
-                if (nums[i]+nums[j] == target):
+                if (nums[i]+nums[j] == target):            #Brute Force Method
                     return [i,j]
         return [ ] 
