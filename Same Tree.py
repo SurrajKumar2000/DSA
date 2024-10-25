b30648fdd000e52b7484e39d5af3ccf7471ec5d1
SK <itzmesk2000@gmail.com>
2024-10-24 21:35:09 -0700

    Update Same Tree.py

--- Same Tree.py
+++ Same Tree.py
@@ -4,6 +4,7 @@ class Solution:
             return True
         if not p or not q or p.val!=q.val:
             return False
-        return ()
+        return (self.isSameTree(p.left, q.left)) and (self.isSameTree(p.right, q.right))
+
         
      
