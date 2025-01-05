3600123b51739322b1ae5d31be8a0cd8dd794da0
Surrajkumar Prabhu Venkatesh <itzmesk2000@gmail.com>
2025-01-05 06:37:22 +0530

    Updated file

--- Pattern Printing/Star Pattern.py
+++ Pattern Printing/Star Pattern.py
@@ -1,5 +1,5 @@
-num = int(input("Enter number:"))
-for i in range(1,num+1):
-    for j in range(1,i+1):
-        print("*", end="")
+n = int(input("Enter the number:"))
+for i in range (n):
+    for j in range(i+1):
+        print("*",end=" ")
     print()
