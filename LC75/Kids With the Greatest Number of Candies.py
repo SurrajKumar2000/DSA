bd7a0c56e1103ccadc1f41ecd4a4086358598286
Surrajkumar Prabhu Venkatesh <itzmesk2000@gmail.com>
2025-01-16 23:00:36 +0530

    Updated file

--- LC75/Kids With the Greatest Number of Candies.py
+++ LC75/Kids With the Greatest Number of Candies.py
@@ -1,3 +1,5 @@
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
-        
+        Max_Candies = max(candies)
+
+        return [(curNum+extraCandies>=Max_Candies) for curNum in candies]        
