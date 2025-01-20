c74585677fb3bd82a410945d66f7cc661c2a492e
Surrajkumar Prabhu Venkatesh <itzmesk2000@gmail.com>
2025-01-20 23:16:01 +0530

    Updated file

--- LC75/Find the Highest Altitude.py
+++ LC75/Find the Highest Altitude.py
@@ -1,1 +1,8 @@
-   
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        high = 0
+        current = 0
+        for i in range (len(gain)):
+            current+=gain[i]
+            high = max(high,current)
+        return high   
