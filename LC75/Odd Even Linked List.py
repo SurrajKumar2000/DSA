6daff32bd8219e8cc4ffdbaf8bef4f636b116fd5
Surrajkumar Prabhu Venkatesh <itzmesk2000@gmail.com>
2025-01-29 09:33:50 +0530

    Updated file

--- LC75/Odd Even Linked List.py
+++ LC75/Odd Even Linked List.py
@@ -1,3 +1,16 @@
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-  
+        
+        if not head or not head.next:
+            return head
+
+        odd = head
+        even = even_head = head.next
+        
+        while even and even.next:
+            odd.next = even.next
+            odd = odd.next
+            even.next = odd.next
+            even = even.next
+            odd.next = even_head
+        return head  
