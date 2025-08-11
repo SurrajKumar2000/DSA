2c78a8e7e32ba4efa78a239c61b236a2f69ec348
Surrajkumar Prabhu Venkatesh <itzmesk2000@gmail.com>
2025-08-11 09:54:42 +0530

    Updated file

--- LC75/Single Number.py
+++ LC75/Single Number.py
@@ -1,6 +1,9 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        res = 0
-        for num in nums:
-            res^=num
-        return res
+        d = {}
+        for i in nums:
+            if i not in d:
+                d[i]=1
+            else:
+                del d[i]
+        return list(d.keys())[0]
